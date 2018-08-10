495116fd2b04161df3ed61bf1d2ab2cfd887a360
Michael Donovan <mike@eggborne.com>
2018-08-10 14:28:06 -0700

    Add template for question card

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 	$('#start-button').click(function(){
 		console.log("start clicked!")
+		$('#question-card-1').show();
+		$('#overlay').show();
+	})
+	$('.yes-button').click(function(){
+		console.log(this + " clicked!")
+	})
+	$('.no-button').click(function(){
+		console.log(this + " clicked!")
 	})
 })
