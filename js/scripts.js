aaa7c70a96cff510046928d4c83a7414606404ad
Michael Donovan <mike@eggborne.com>
2018-08-10 21:23:55 -0700

    Fix typo to enable correct scorekeeping

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,8 @@ $(document).ready(function(){
 })
 function updateTrackScores(newData) {
 	for (var weight in newData) {
+		console.log(weight)
+		console.log(newData[weight])
 		trackScores[weight] += newData[weight]
 	}
 	console.log("score now")
@@ -24,15 +26,17 @@ function Card(index,hide) {
 	index = index.toString()
 	this.questionObject = questions[index]
 	this.divID = "question-card-"+index
-	this.html = `<div id="`+this.divID+`" class="panel panel-default question-card"> <div class="panel-heading"> <h3>Question `+(cards.length+1)+` of `+Object.keys(questions).length+`</h3> </div> <div class="panel-body"><h3 class="well">`+this.questionObject.query+`</h3><div class="panel-footer">  <button id="left-button-`+index+`" type="button" class="btn btn-success btn-wd left-button">`+this.questionObject.leftResponse.text+`</button> <button id="right-button-`+index+`" type="button" class="btn btn-info btn-wd right-button">`+this.questionObject.rightResponse.text+`</button></div></div>`
+	this.html = `<div id="`+this.divID+`" class="panel panel-default question-card"> <div class="panel-heading"> <h3>Question `+(cards.length+1)+` of `+Object.keys(questions).length+`</h3> </div> <div class="panel-body"><h3 class="well" style="padding:50px">`+this.questionObject.query+`</h3><div class="panel-footer">  <button id="left-button-`+index+`" type="button" class="btn btn-success btn-wd left-button">`+this.questionObject.leftResponse.text+`</button> <button id="right-button-`+index+`" type="button" class="btn btn-info btn-wd right-button">`+this.questionObject.rightResponse.text+`</button></div></div>`
 	$('#card-area').html(this.html)
 	$('#left-button-'+index).click(function(){
 		var index = this.id[this.id.length-1]
+		console.log("set card left but index to " + index)
 		submitResponse(index,"left")
 	})
 	$('#right-button-'+index).click(function(){
 		var index = this.id[this.id.length-1]
-		submitResponse(index,"left")
+		console.log("set card right but index to " + index)
+		submitResponse(index,"right")
 	})
 	this.questionObject.cardObject = this
 	this.easeIn = function() {
@@ -47,6 +51,7 @@ function Card(index,hide) {
 function submitResponse(buttonIndex,side) {
 	var questionObj = questions[buttonIndex]
 	var action = questionObj[side+"Action"]
+	console.log("performing " + (side+"Action") + " of " + buttonIndex)
 	action()
 }
 function produceResult() {
