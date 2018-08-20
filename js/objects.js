af1c9b83af1332831af4afa798844020523e0d2b
Michael Donovan <mike@eggborne.com>
2018-08-19 18:26:44 -0700

    Move .weights property to questions object

--- js/objects.js
+++ js/objects.js
@@ -3,21 +3,18 @@ var tracks = {
 		score: 0,
 		displayName: "Ruby/Rails",
 		description: "A nice succinct description for Ruby/Rails",
-		weights: [[1,1],[1,1],[1,1],[1,1],[1,2]],
 		color: {hex:"#ff4444",name:"danger"}
 	},
 	'react':{
 		score: 0,
 		displayName: "CSS/React",
 		description: "A nice succinct description for CSS/React",
-		weights: [[2,0],[0,2],[3,0],[3,0],[0,1]],
 		color: {hex:"#ffbb33",name:"warning"}
 	},
 	'net':{
 		score: 0,
 		displayName: "C#/.NET",
 		description: "A nice succinct description for C#/.NET",
-		weights: [[0,2],[2,0],[0,2],[0,4],[1,0]],
 		color: {hex:"#00C851",name:"success"}
 	}
 }
@@ -26,45 +23,55 @@ var questions = [
 		query: "Would you rather be an interior decorator or an architect?",
 		leftResponse: {
 			text: "Interior Decorator",
+			weights: [1,2,0] // [rails,react,net]
 		},
 		rightResponse: {
 			text: "Architect",
+			weights: [1,0,2]
 		}
 	},
 	{
 		query: "Who's cooler: Bill Gates or Willem Dafoe?",
 		leftResponse: {
-			text: "Bill Gates"
+			text: "Bill Gates",
+			weights: [1,0,2]
 		},
 		rightResponse: {
-			text: "Willem Dafoe"
+			text: "Willem Dafoe",
+			weights: [1,2,0]
 		}
 	},
 	{
 		query: "Do you straighten the pictures in hotel rooms?",
 		leftResponse: {
-			text: "Of course"
+			text: "Of course",
+			weights: [1,2,0]
 		},
 		rightResponse: {
-			text: "No way"
+			text: "No way",
+			weights: [1,0,2]
 		}
 	},
 	{
 		query: "Complete this sentence: \"It really grinds my gears when things...\"",
 		leftResponse: {
-			text: "Don't look right"
+			text: "Don't look right",
+			weights: [1,3,0]
 		},
 		rightResponse: {
-			text: "Don't work right"
+			text: "Don't work right",
+			weights: [2,0,3]
 		},
 	},
 	{
 		query: "John Lennon or Paul McCartney?",
 		leftResponse: {
 			text: "John",
+			weights: [1,0,2]
 		},
 		rightResponse: {
 			text: "Paul",
+			weights: [2,1,0]
 		}
 	}
 ]
